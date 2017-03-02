22d7a80ced7c25ed4bd9d1ea3031092ca42a9584
sramirez <sramirez@solidgear.es>
2017-03-02 18:29:12 +0100

    Styled Custom Card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,11 +23,11 @@ export class AppComponent {
   private section_title = 'USERS';
 
   private users = [
-    new User("1", "Bart Simpson", "Always up to no good", "bart-simpson.png"),
-    new User("2", "Lisa Simpson", "The best of his class", "lisa-simpson.png"),
-    new User("3", "Homer Simpson", "D'oh!", "homer-simpson.png"),
-    new User("4", "Marge Simpson", "Great wife", "marge-simpson.png"),
-    new User("5", "Milhouse", "Nobody likes Milhouse!", "milhouse.jpeg"),
-    new User("6", "Ppal. Skinner", "I love my mum", "skinner.png"),
+    new User("1", "Bart Simpson", "Always up to no good", "./resources/images/bart-simpson.png"),
+    new User("2", "Lisa Simpson", "The best of his class", "./resources/images/lisa-simpson.png"),
+    new User("3", "Homer Simpson", "D'oh!", "./resources/images/homer-simpson.png"),
+    new User("4", "Marge Simpson", "Great wife", "./resources/images/marge-simpson.png"),
+    new User("5", "Milhouse", "Nobody likes Milhouse!", "./resources/images/milhouse.jpeg"),
+    new User("6", "Ppal. Skinner", "I love my mum", "./resources/images/skinner.png"),
   ]
 }
